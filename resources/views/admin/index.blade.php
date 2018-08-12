9028a8973429ff6e70b6a4763994aaf234032975
Bruno Onuki <bruno_guilherme_95@hotmail.com>
2018-08-11 23:39:30 -0300

    alterações 11/08/2018

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+
+@section('title', 'Registrar Usuário')
+
+@section('content_header')
+    <h1>Registrar Usuário</h1>
+
+    <ol class="breadcrumb">
+        <li> <a href=""> DashBoard</a> </li>
+        <li> <a href=""> Registrar usuário</a> </li>
+        <li> <a href=""> Registrar prestador de serviços</a> </li>
+
+    </ol>
+@stop
+
+@section('content')
+    <p>Deu certo</p>
+@stop
